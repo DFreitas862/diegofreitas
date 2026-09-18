aa05936f255f490f31d92c875ea05de78f9faeaf
DFreitas862 <diegofreitaslw35@gmail.com>
2026-09-18 20:34:16 -0300

    supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,5 @@
+// Cole suas chaves do Supabase aqui
+const SUPABASE_URL = 'COLE_SUA_URL_AQUI';
+const SUPABASE_ANON_KEY = 'COLE_SUA_ANON_KEY_AQUI';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
