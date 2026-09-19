0bb830a9d2397a3dc1153fc362b25eb7214e3a9d
DFreitas862 <diegofreitaslw35@gmail.com>
2026-09-18 21:25:48 -0300

    Delete minha-agenda directory

--- minha-agenda/js/supabase.js
+++
@@ -1,5 +0,0 @@
-// Cole suas chaves do Supabase aqui
-const SUPABASE_URL = 'https://cqeazscoemjlixhlazpi.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_oBeDiUHvYt4ZAZ6WvRz4rw_slF17m03';
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
